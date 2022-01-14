1d68d4922b634d1f11110419068158f26d727286
willd <willdwelle@frontier.com>
2022-01-14 12:47:10 -0800

    3 boxes in line

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <BoxForm/>
-      <BoxDisplay box={ currentBox }/>
+      <BoxDisplay />
     </div>
   );
 }
--- src/components/displayBox.jsx
+++ src/components/displayBox.jsx
@@ -1,9 +1,12 @@
 import react, { useState } from 'react';
 
+
 const BoxDisplay = (props) => {
     return (
-        <div>
-            
+        <div style={{display: 'inline-flex'}}>
+            <div className='box1' style={{height: '40px', width: '40px', backgroundColor: 'red', border: 'solid', margin: '15px'}} ></div>
+            <div className='box2' style={{height: '40px', width: '40px', backgroundColor: 'red', border: 'solid', margin: '15px'}} ></div>
+            <div className='box3' style={{height: '40px', width: '40px', backgroundColor: 'red', border: 'solid', margin: '15px'}} ></div>
         </div>
     );
 };
